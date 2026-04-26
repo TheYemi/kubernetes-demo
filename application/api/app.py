f55142c1d574cb262c5e04dcd9152d8361f806b7
Opeyemi Olasehinde <olasehindeo3@gmail.com>
2026-04-26 18:43:20 +0100

    testing trigger multi-environment deployment pipeline

--- application/api/app.py
+++ application/api/app.py
@@ -45,3 +45,4 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # nosemgrep: python.flask.security.audit.app-run-param-config.avoid_app_run_with_bad_host
 
 # SAST test trigger# Test coverage enforcement
+# Stage 3 test
